a6ac84c075c27bc6c9c226ca07be9a22d0c24ffc
qixing-ai <805407646@qq.com>
2025-06-04 01:58:48 +0000

    适配pytorch

--- diff-surfel-rasterization/setup.py
+++ diff-surfel-rasterization/setup.py
@@ -27,7 +27,13 @@ setup(
             "cuda_rasterizer/backward.cu",
             "rasterize_points.cu",
             "ext.cpp"],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
+            extra_compile_args={
+                "nvcc": [
+                    "-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/"),
+                    "--std=c++17"
+                ],
+                "cxx": ["-std=c++17"]
+            })
         ],
     cmdclass={
         'build_ext': BuildExtension
